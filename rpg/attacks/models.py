37762fda3d4c13c05a3200380351e4627af9da2f
Anthony Keating <anthonyk1225@gmail.com>
2015-04-10 18:57:47 -0400

    added models

--- rpg/attacks/models.py
+++ rpg/attacks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from models.character import Character
 
 # Create your models here.
+class Attack(models.Model):
+    name = models.CharField(max_length=40,unique=True)
+    
--- rpg/characters/models.py
+++ rpg/characters/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
+from attacks.models import Attack
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=40,unique=True)
+    class_ = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    attack = models.ManyToManyField(Attack)
+    user = models.ForeignKey(User)
+
+class Atttributes(models.Model):
+	hit_points = models.IntegerField()
+	attack = models.IntegerField()
+	defense = models.IntegerField()
+	character = models.ForeignKey(Character)

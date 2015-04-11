a4635b3474e6e7d86178fae9a71b7535d1c796a5
Anthony Keating <anthonyk1225@gmail.com>
2015-04-11 16:32:14 -0400

    can now create a char with stats

--- rpg/characters/views.py
+++ rpg/characters/views.py
@@ -6,15 +6,18 @@ from users.models import User
 # Create your views here.
 def create_warrior(request):
 	current_user = User.objects.filter(id=request.session['user_id'])
-	new_char = Character(race='warrior', name=request.POST['name'], user=current_user[0])
-	return redirect('/rpg/')
+	new_char = Character.objects.create(race='warrior', name=request.POST['name'], user=current_user[0])
+	Attribute.objects.create(hit_points=40,attack=5,character=new_char)
+	return redirect('/rpg/welcome/')
 
 def create_mage(request):
 	current_user = User.objects.filter(id=request.session['user_id'])
-	new_char = Character(race='mage', name=request.POST['name'], user=current_user[0])
-	return redirect('rpg')
+	new_char = Character.objects.create(race='mage', name=request.POST['name'], user=current_user[0])
+	Attribute.objects.create(hit_points=22,attack=8,character=new_char)
+	return redirect('rpg/welcome/')
 
 def create_paladin(request):
 	current_user = User.objects.filter(id=request.session['user_id'])
-	new_char = Character(race='paladin', name=request.POST['name'], user=current_user[0])
-	return redirect('rpg')
+	new_char = Character.objects.create(race='paladin', name=request.POST['name'], user=current_user[0])
+	Attribute.objects.create(hit_points=54,attack=3,character=new_char)
+	return redirect('rpg/welcome/')

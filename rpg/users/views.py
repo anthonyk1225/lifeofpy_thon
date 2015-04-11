565e0bc9b7bab8f0ba8618f1572ada6040259384
Anthony Keating <anthonyk1225@gmail.com>
2015-04-11 00:27:57 -0400

    setting up welcome screen. logout added which flushes session key

--- rpg/users/views.py
+++ rpg/users/views.py
@@ -25,9 +25,14 @@ class LogInView(View):
         if len(user) == 1 and check_password(password,user[0].password):
             user[0].save()
             request.session['user_id']=user[0].id
-            return redirect('/users/welcome_page/')
+            return redirect('/rpg/welcome/')
         return render(request, self.template_name,{'error': 'Invalid Username or Password'})
     
+class LogOutView(View):
+    def get(self, request):
+        request.session.flush()
+        return redirect('/rpg/')
+
 class RegisterView(View):
     form_class = UserForm
     template_name = 'users/register.html'
@@ -42,15 +47,19 @@ class RegisterView(View):
             user = User(username=request.POST['username'], password=hashed)
             user.save()
             request.session['user_id'] = user.id
-            return render(request, 'users/welcome.html',{'username': user.username})
-        return redirect('/users/log_in_page/')
+            return render(request, 'rpg/welcome.html',{'username': user.username})
+        return redirect('/rpg/log_in/')
 
 class WelcomeView(View):
     template = 'users/welcome.html'
 
     def get(self, request):
         if not request.session.get('user_id', False):
-            return redirect('/users/')
-        request.session.set_expiry(120)
+            return redirect('/rpg/')
+        # request.session.set_expiry(120)
         user = User.objects.get(id=request.session['user_id'])
         return render(request, self.template, {'username': user.username})
+
+
+
+

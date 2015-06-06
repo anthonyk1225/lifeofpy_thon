362e811881cc1cd9f511bb505ed8886e2f35a3a2
Thomas Harvey <thomasharvey24@gmail.com>
2015-06-05 20:07:21 -0400

    Moved images to static directory. Removed attacks/seed

--- attacks/seed.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-from attacks.models import Attack
-
-r = requests.get('http://pokemondb.net/move/all')
-soup = BeautifulSoup(r.text)
-[for row in soup.table.tbody]
